c8c5323e1129dcd9281dca6904e0791db13e2750
clemens <clemens@vibee.de>
2015-01-12 00:35:25 +0100

    Initial commit

---
+++ mpd-ps.py
@@ -0,0 +1,197 @@
+__author__ = 'Clemens Hoffmann [clemens [at] vibee.de'
+
+from mpd import MPDClient
+import os
+import shutil
+import time
+import logging
+import argparse
+
+def remove_empty_dirs(path):
+    for root, dirnames, filenames in os.walk(path, topdown=False):
+        for dirname in dirnames:
+            for dirpath, dirnames2, filenames2 in os.walk(os.path.join(root, dirname)):
+                if not filenames2 and not dirnames2:
+                    logger.info(os.path.join(root, dirpath) + " is empty. Deleting empty directory.")
+                    os.rmdir(os.path.join(root, dirpath))
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--encoder", help="Specify manual encoder settings (e.g. lame -V2 or oggenc -q4). The string is parsed "
+                                      "directly to bash. Default: oggenc -q4 --resample 44100",
+                    dest="encoder")
+parser.add_argument("--copy-flac", help="copy flac files instead of transcoding them",
+                    action="store_true")
+parser.add_argument("--threads", help="Amount of parallel encoding processes when transcoding flac files. Default: Auto-detect", type=int,
+                    dest="threads")
+parser.add_argument("--host", help="adress of mpd server. Default: localhost",
+                    dest="host")
+parser.add_argument("--port", help="port of mpd server. Default: 6600", type=int,
+                    dest="port")
+parser.add_argument("--password", help="password of mpd server.",
+                    dest="password")
+parser.add_argument("--delete-non-existent", help="delete files from destination which are not in mpd playlist. Also deletes empty "
+                                                  "directories in destination folder", action="store_true")
+parser.add_argument("--dont-copy-album-art", help="do not copy .jpg and .png album art to destination.",
+                    action="store_true")
+
+parser.add_argument("mpd_music_folder", metavar="mpd-music-folder", nargs=1, help="root folder of mpd server")
+parser.add_argument("destination_folder", metavar="destination-folder", nargs=1, help="folder where the audio files are copied / transcoded to")
+
+args = parser.parse_args()
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("mpd-ps")
+
+if args.mpd_music_folder:
+    mpd_root_dir = args.mpd_music_folder[0]
+else:
+    logger.error("Please specify the root folder of mpd server with -in parameter.")
+    exit()
+
+if args.destination_folder:
+    dest_dir = args.destination_folder[0]
+else:
+    logger.error("Please specify the output folder with -out parameter.")
+    exit()
+
+if args.encoder:
+    encoder = args.encoder
+else:
+    encoder = "oggenc -q4 --resample 44100"
+
+if args.copy_flac:
+    copy_flac = True
+else:
+    copy_flac = False
+
+if args.host:
+    host = args.host
+else:
+    host = "localhost"
+
+if args.port:
+    port = args.port
+else:
+    port = 6600
+
+if args.password:
+    password = args.password
+else:
+    password = ""
+
+if args.threads and args.threads > 0:
+    threads = "j" + args.threads
+else:
+    threads = ""
+
+current_milli_time = lambda: int(round(time.time() * 1000))
+
+client = MPDClient()
+client.timeout = 10
+client.idletimeout = None          # timeout for fetching the result of the idle command is handled seperately, default: None
+
+client.connect(host, port)  # connect to localhost:6600
+client.password(password)
+
+playlist = client.playlist()          # print the MPD version
+client.close()                     # send the close command
+client.disconnect()                # disconnect from the server
+
+flac_files = {}
+size = 0
+mytime = 0
+added_files = set()
+folders = {}
+count = 0
+
+for item in playlist:
+
+    src_full_path = os.path.join(mpd_root_dir,item[6:]) #exclude "file: "
+    src_path = src_full_path[:src_full_path.rfind('/')+1] #stripped filename
+    item_short_path = item[:item.rfind('/')+1] #relative path without filename
+
+    dest_full_path = os.path.join(dest_dir,item[6:]) #exclude "file " (: replaced by re.sub()
+    dest_path = dest_full_path[:dest_full_path.rfind('/')+1]
+    filename = dest_full_path[dest_full_path.rfind('/')+1:]
+
+    folders[src_path] = dest_path
+
+    # Skip existing files
+    if os.path.isfile(dest_full_path) and os.path.getsize(dest_full_path) == os.path.getsize(src_full_path):
+        logger.info("file " + dest_full_path + " exists. Skipping.")
+        added_files.add(dest_full_path)
+        continue
+
+    # Create directories on destination, if they don't exist
+    if not os.path.exists(dest_path):
+        os.makedirs(dest_path)
+
+    # Transcode flac files to ogg vorbis q4 on destination
+    if filename[filename.rfind('.')+1:] == "flac" and not copy_flac:
+        os.environ ['infile'] = src_full_path
+        transcoded_file = dest_full_path[:dest_full_path.rfind('.')] + ".ogg"
+        added_files.add(transcoded_file)
+        os.environ['outfile'] = transcoded_file
+        if not os.path.exists(transcoded_file):
+            if item_short_path[6:] in flac_files:
+
+                flac_files[item_short_path[6:]].append(item[6:])
+
+            else:
+                flac_files[item_short_path[6:]] = []
+                flac_files[item_short_path[6:]].append(item[6:])
+
+        else:
+            logger.info("Transcoded file exists: " + transcoded_file)
+
+    # Copy non-flac media files directly to destination
+    else:
+        added_files.add(dest_full_path)
+        cur_time = current_milli_time()
+        try:
+            shutil.copy(src_full_path,dest_full_path)
+            count+=1
+            logger.info("Copied file to: " + dest_full_path)
+
+            mytime += (current_milli_time() - cur_time) / 1000
+            size += os.path.getsize(dest_full_path) / (1024*1024)
+            if mytime != 0:
+                print("Transferred " + str(count) + " files with " + str(size / mytime) + "MB/s")
+        except OSError:
+            logger.error("Copying " + src_full_path + " to " + dest_full_path + " failed. Skipping file.")
+
+print("Transferred " + str(size) + " MB.")
+
+if flac_files:
+    print("Start transcoding flac files now.")
+    for list in flac_files:
+        os.system("parallel " + threads + " " + encoder + " \"" + mpd_root_dir + "/{}\" -o \"" + dest_dir + "/{.}.ogg\" ::: \"" + '" "'.join(flac_files[list]) + "\"")
+        #find -name "*.flac" | parallel -q oggenc -q4 {} -o /tmp/oggout/'{= s/replace/by/g =}'
+
+print("Start Copying album art now.")
+# Copying image files (jpg,png) to destination (covers etc)
+
+if not args.dont_copy_album_art:
+    for src in folders:
+        for file in os.listdir(src):
+            if file.endswith(".jpg") or file.endswith(".png"):
+                added_files.add(folders[src] + file)
+                if not os.path.isfile(os.path.join(folders[src],file)):
+                    shutil.copy(src+file,os.path.join(folders[src],file))
+                    logger.info("Copies image: " + file + " to " + folders[src])
+                else:
+                    logger.info("Cover exists: " + folders[src] + file)
+
+if args.delete_non_existent:
+    print("Start removing files now.")
+    # Remove files from destination that are not in playlist any more
+    dest_files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(dest_dir) for f in filenames]
+    for f in dest_files:
+        if not f in added_files:
+            logger.info("File " + f + " is not in playlist any more. Removing it.")
+            os.remove(f)
+    # Remove potential empty directories after deleting stuff.
+    remove_empty_dirs(dest_dir)
+
+
